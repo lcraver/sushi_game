ebc4f56e0b9185a88ff066a6347fd257fcd40497
Liam Craver <studios.lime@gmail.com>
2016-10-21 18:52:29 -0500

    Added basic player controller stuff.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    public Dictionary<Tentacle.Type, Tentacle> tentacles = new Dictionary<Tentacle.Type, Tentacle>();
+
+    public Rigidbody2D rb;
+    public float lastRotation;
+    public float currRotation;
+    public float currentAngularSpeed;
+    public float maxAngularSpeed = 5f;
+    public float speedMult = 2f;
+    public float slowDownMult = 2f;
+
+    void Start ()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        //Tentacle tentacle = new Tentacle(this.gameObject, Tentacle.Type.walking1);
+    }
+
+    void FixedUpdate()
+    {
+        currRotation = rb.rotation;
+        currentAngularSpeed = rb.angularVelocity;
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0)
+            rb.AddTorque(-horizontal * speedMult);
+        else if (horizontal < 0)
+            rb.AddTorque(-horizontal * speedMult);
+        else
+        {
+            rb.AddTorque(slowDownMult * (lastRotation - currRotation));
+        }
+
+        if (Mathf.Abs(currentAngularSpeed) > maxAngularSpeed)
+            rb.AddTorque(lastRotation - currRotation);
+
+        lastRotation = currRotation;
+    }
+}
---
+++ Assets/Tentacle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tentacle : MonoBehaviour {
+
+    GameObject player;
+    public GameObject tentacleObject;
+
+    public GameObject walkingPrefab;
+    public GameObject blockbreakPrefab;
+    public GameObject blockpullPrefab;
+    public GameObject detachPrefab;
+
+    public enum Type { walking1, walking2, blockbreak, blockpull, detach1, detach2 };
+    public Type type = Type.walking1;
+    
+    public Tentacle(GameObject _player, Type _type)
+    {
+        type = _type;
+        player = _player;
+
+        switch(_type)
+        {
+            case Type.walking1:
+                tentacleObject = Instantiate(walkingPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+            case Type.walking2:
+                tentacleObject = Instantiate(walkingPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+            case Type.blockbreak:
+                tentacleObject = Instantiate(blockbreakPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+            case Type.blockpull:
+                tentacleObject = Instantiate(blockpullPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+            case Type.detach1:
+                tentacleObject = Instantiate(detachPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+            case Type.detach2:
+                tentacleObject = Instantiate(detachPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                break;
+        }
+
+        player.GetComponent<PlayerController>().tentacles.Add(_type, this);
+    }
+}
