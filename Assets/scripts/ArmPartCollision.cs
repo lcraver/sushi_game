bae4465c52d700ee8bdcc896c3903e2cdce080c7
Liam Craver <studios.lime@gmail.com>
2016-10-21 23:02:57 -0500

    Cleaned up code

---
+++ Assets/scripts/ArmPartCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmPartCollision : MonoBehaviour {
+
+    public GameObject tentacleMaster;
+
+    void Awake()
+    {
+        tentacleMaster = this.transform.parent.parent.gameObject;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" && !tentacleMaster.GetComponent<CollectableTentacle>().collected)
+        {
+            PlayerController.inst.CreateTentacle(tentacleMaster.GetComponent<CollectableTentacle>().type, tentacleMaster.GetComponent<CollectableTentacle>().rot);
+            Destroy(tentacleMaster);
+        }
+    }
+}
---
+++ Assets/scripts/CollectableTentacle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableTentacle : MonoBehaviour {
+
+    public bool collected = false;
+    public Tentacle.Type type;
+    public float rot;
+}
